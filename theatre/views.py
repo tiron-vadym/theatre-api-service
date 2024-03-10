02d584a226146359ce2b794c44f526fdcb464d04
Vadym Tiron <tironvadim1@gmail.com>
2024-03-10 18:14:27 +0200

    add views.py

---
+++ theatre/views.py
@@ -0,0 +1,91 @@
+from django.shortcuts import render
+
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.authentication import TokenAuthentication
+
+from .models import (
+    Play,
+    Actor,
+    Genre,
+    TheatreHall,
+    Performance,
+    Reservation,
+    Ticket,
+)
+from .serializers import (
+    PlaySerializer,
+    PlayerListSerializer,
+    PlayerDetailSerializer,
+    ActorSerializer,
+    GenreSerializer,
+    TheatreHallSerializer,
+    PerformanceSerializer,
+    ReservationSerializer,
+    TicketSerializer,
+    TicketListSerializer,
+    TicketDetailSerializer,
+)
+from .permissions import IsAdminOrIfAuthenticatedReadOnly
+
+
+class PlayViewSet(GenericViewSet):
+    queryset = Play.objects.all().prefetch_related("actors", "genres")
+    serializer_class = PlaySerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PlayListSerializer
+        if self.action == "retrieve":
+            return PlayDetailSerializer
+        return PlaySerializer
+
+
+class ActorViewSet(GenericViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class GenreViewSet(GenericViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class TheatreHallViewSet(GenericViewSet):
+    queryset = TheatreHall.objects.all()
+    serializer_class = TheatreHallSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class PerformanceViewSet(GenericViewSet):
+    queryset = Performance.objects.all().select_related("play")
+    serializer_class = PerformanceSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class ReservationViewSet(GenericViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class TicketViewSet(GenericViewSet):
+    queryset = Ticket.objects.all().select_related("performance", "reservation")
+    serializer_class = TicketSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return TicketListSerializer
+        if self.action == "retrieve":
+            return TicketDetailSerializer
+        return TicketSerializer
