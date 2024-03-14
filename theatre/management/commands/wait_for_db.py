22cb338757aa599c0ec750411e94e682328abad4
Vadym Tiron <tironvadim1@gmail.com>
2024-03-14 20:43:40 +0200

    fix mistakes and add migrations

--- theatre/management/commands/wait_for_db.py
+++ theatre/management/commands/wait_for_db.py
@@ -9,12 +9,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write("Waiting for database...")
-        db_connection = None
-        while not db_connection:
+        db_conn = None
+        while not db_conn:
             try:
-                connection.ensure_connection()
-                db_connection = True
-                self.stdout.write(self.style.SUCCESS("Database available!"))
+                db_conn = connections["default"]
             except OperationalError:
                 self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("Database available!"))
